8eeb5314d429d29d6a1dc69f9e8cf69468380c63
诗云 <15876781@qq.com>
2022-02-28 16:56:07 +0800

    C6T6空白工程

---
+++ project/App/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: 诗云
+ * @Date: 2022-02-28 10:22:38
+ * @LastEditors: 诗云
+ * @LastEditTime: 2022-02-28 14:49:42
+ * @FilePath: \22GDproject\project\App\src\main.c
+ * @Description: 主函数
+ * 
+ */
+#include "stm32f10x.h"
+#include "oled.h"
+#include "delay.h"
+
+int main(void)
+{
+	//初始化延时函数
+	delay_init();
+	//初始化OLED
+	OLED_init();
+	
+	
+	return 0;
+}
